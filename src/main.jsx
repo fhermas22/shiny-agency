fc7081f978a626b7d5775bb9e6e9d88133babc2d
Hermas Francisco <franciscohermas@gmail.com>
2026-01-19 16:40:43 +0100

    Complete Pratice P1C2 : Create new pages Results & Freelances, and their routes in main.jsx; Update Header component and Survey page;

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router'
 import './index.css'
 import Home from './pages/Home/index.jsx'
 import Survey from './pages/Survey/index.jsx'
+import Freelances from './pages/Freelances/index.jsx'
+import Results from './pages/Results/index.jsx'
 import Header from './components/Header/index.jsx'
 import Error from './components/Error/index.jsx'
 
@@ -14,6 +16,8 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/survey/:questionNumber" element={<Survey />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/freelances" element={<Freelances />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
