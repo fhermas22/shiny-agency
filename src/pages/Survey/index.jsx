58b0bfe660f8bfa7473939cde38d52b12c906962
Hermas Francisco <franciscohermas@gmail.com>
2026-01-20 14:21:38 +0100

    Complete Pratice P2C1 : Implement API calls logic for Survey page

--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -31,41 +31,53 @@ const LinkWrapper = styled.div`
 `
 
 function Survey() {
-    const { questionNumber } = useParams()
-    const questionNumberInt = parseInt(questionNumber)
-    const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
-    const nextQuestionNumber = questionNumberInt + 1
-    const [surveyData, setSurveyData] = useState({})
-    const [isDataLoading, setDataLoading] = useState(false)
+  const { questionNumber } = useParams()
+  const questionNumberInt = parseInt(questionNumber)
+  const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
+  const nextQuestionNumber = questionNumberInt + 1
+  const [surveyData, setSurveyData] = useState({})
+  const [isDataLoading, setDataLoading] = useState(false)
+  const [error, setError] = useState(false)
 
-    useEffect(() => {
-        setDataLoading(true)
-        fetch(`http://localhost:8000/survey`)
-            .then((response) => response.json())
-            .then(({ surveyData }) => {
-                setSurveyData(surveyData)
-                setDataLoading(false)
-            })
-    }, [])
+  useEffect(() => {
+    async function fetchSurvey() {
+      setDataLoading(true)
+      try {
+        const response = await fetch(`http://localhost:8000/survey`)
+        const { surveyData } = await response.json()
+        setSurveyData(surveyData)
+      } catch (err) {
+        console.log(err)
+        setError(true)
+      } finally {
+        setDataLoading(false)
+      }
+    }
+    fetchSurvey()
+  }, [])
 
-    return (
-        <SurveyContainer>
-            <QuestionTitle>Question {questionNumber}</QuestionTitle>
-            {isDataLoading ? (
-                <Loader />
-            ) : (
-                <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
-            )}
-            <LinkWrapper>
-                <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
-                {surveyData[questionNumberInt + 1] ? (
-                    <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
-                ) : (
-                    <Link to="/results">Résultats</Link>
-                )}
-            </LinkWrapper>
-        </SurveyContainer>
-    )
+  if (error) {
+    return <span>Oups il y a eu un problème</span>
+  }
+
+  return (
+    <SurveyContainer>
+      <QuestionTitle>Question {questionNumber}</QuestionTitle>
+      {isDataLoading ? (
+        <Loader />
+      ) : (
+        <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
+      )}
+      <LinkWrapper>
+        <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+        {surveyData[questionNumberInt + 1] ? (
+          <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+        ) : (
+          <Link to="/results">Résultats</Link>
+        )}
+      </LinkWrapper>
+    </SurveyContainer>
+  )
 }
 
 export default Survey
