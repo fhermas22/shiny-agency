8af1acd7ad71ce342bb176d92e99ad34137b826a
Hermas Francisco <franciscohermas@gmail.com>
2026-01-20 12:02:09 +0100

    Scope our CSS with styled-components

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
-import './index.css'
 import Home from './pages/Home/index.jsx'
 import Survey from './pages/Survey/index.jsx'
 import Freelances from './pages/Freelances/index.jsx'
 import Results from './pages/Results/index.jsx'
 import Header from './components/Header/index.jsx'
 import Error from './components/Error/index.jsx'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    div {
+        font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+`
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
+      <GlobalStyle />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components'
 import Card from '../../components/Card'
 
 const freelanceProfiles = [
@@ -15,17 +16,26 @@ const freelanceProfiles = [
     },
 ]
 
+const CardsContainer = styled.div`
+    display: grid;
+    gap: 24px;
+    grid-template-rows: 350px 350px;
+    grid-template-columns: repeat(2, 1fr);
+`
+
 function Freelances() {
     return (
         <div>
             <h1>Freelances 👩·💻👨·💻👩·💻</h1>
-            {freelanceProfiles.map((profile, index) => (
-                <Card
-                    key={`${profile.name}-${index}`}
-                    label={profile.jobTitle}
-                    title={profile.name}
-                />
-            ))}
+            <CardsContainer>
+                {freelanceProfiles.map((profile, index) => (
+                    <Card
+                        key={`${profile.name}-${index}`}
+                        label={profile.jobTitle}
+                        title={profile.name}
+                    />
+                ))}
+            </CardsContainer>
         </div>
     )
 }
